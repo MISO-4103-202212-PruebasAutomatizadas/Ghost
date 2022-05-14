40500723b4cd05a00553d4659232ffd6dab3664f
Edinson Morales <e.moralesm2@uniandes.edu.co>
2022-05-14 18:20:13 -0500

    Scenarios for Playwright

--- playwrightGhost/index_tags.js
+++ playwrightGhost/index_tags.js
@@ -1,5 +1,9 @@
 const playwright = require("playwright");
 const LoginPage = require("./pages_objects/login.page");
+const DashboardPage = require("./pages_objects/dashboard.page");
+const TagsPage = require('./pages_objects/tags.page');
+const TagNewPage = require('./pages_objects/tags_new.page');
+const TagEditPage = require('./pages_objects/tag_edit.page');
 
 const url = "http://localhost:2368/";
 const urlAdmin = url + "Ghost";
@@ -9,68 +13,149 @@ const adminPass = "abcd1234*$";
 
 (async () => {
   for (const browserType of ["chromium"]) {
-    let feedback = "";
     const browser = await playwright[browserType].launch();
     const context = await browser.newContext();
-    const page = await context.newPage();
+    var page = await context.newPage();
 
-    /* SETTINGS -------------------------------------------------------------- */
+    // SETTINGS
     console.log(("Browser: " + browserType).padEnd(100, "_"));
     console.log("Feature: Gestion de tags de ghost".padEnd(100, "_"));
-    console.log("Description: Como usuario quiero gestionar los tags en ghost para usarse en la creacion de post".padEnd(100, "_"));
+    console.log(
+      "Description: Como usuario quiero gestionar los tags en ghost para usarse en la creacion de post".padEnd(
+        100,
+        "_"
+      )
+    );
     console.log("\r");
 
-    /* ESCENARIO 1 -------------------------------------------------------------- */
-    escenario_1(page);
-
-    /* ESCENARIO 2 -------------------------------------------------------------- */
-    escenario_2(page);
-
-    /* ESCENARIO 3 -------------------------------------------------------------- */
-    //escenario_3(page);
-
-    /* ESCENARIO 4 -------------------------------------------------------------- */
-    //escenario_4(page);
-
-    /* ESCENARIO 5 -------------------------------------------------------------- */
-    //escenario_5(page);
+    // RUN SCENARIOS 1
+    runScenarios(page);
   }
 
-  async function login(page) {
+  async function runScenarios(page) {
+    console.log("_".padEnd(100, "_"));
+    console.log("Scenario: 1. Creación de un tag exitoso".padEnd(100, "_"));
+    console.log("Given I login on Ghost page with <ADMIN1> and <PASSWORD1>".padEnd(100,"_"));
     await page.goto(urlAdmin);
-    if (await page.isVisible(LoginPage.emailInput)) {
+    await page.type(LoginPage.emailInput, userAdmin);
+    await page.type(LoginPage.passwInput, adminPass);
+    await page.click(LoginPage.signInButton);    
+    await page.click(DashboardPage.tagsMenu);
+    await page.click(TagsPage.newTagButton);
+    await page.screenshot({ path: pathReports + "./1.1-loginSuccess.png" });    
+    
+    console.log(`When I create a new tag with tag_1 name and "description_tag_1" description`)
+    TagsPage.tagTest = "tag_1";
+    await page.type(TagNewPage.nameInput, "tag_1");
+    await page.type(TagNewPage.descriptionInput, "description_tag_1");
+    await page.click(TagNewPage.saveButton);
+    console.log("Then I wait for 1 seconds".padEnd(100, "_"));
+    await new Promise(r => setTimeout(r, 1000));
+  
+    ////////////////////////////////////////////////////////////////////////////////////////
+    console.log("\n");
+    console.log("_".padEnd(100, "_"));
+    console.log("Scenario: 2. Encontrar de un tag creado".padEnd(100, "_"));
+    
+    console.log("Given I login on Ghost page with <ADMIN1> and <PASSWORD1>".padEnd(100,"_"));
+    await page.goto(urlAdmin);
+    if(await page.isVisible(LoginPage.emailInput)) {      
+      await page.type(LoginPage.emailInput, userAdmin);
+      await page.type(LoginPage.passwInput, adminPass);
+      await page.click(LoginPage.signInButton);      
+    }
+    
+    console.log(`When I create a new tag with tag_2 name and "description_tag_2" description`);
+    await page.click(DashboardPage.tagsMenu);
+    await page.click(TagsPage.newTagButton);
+    await page.type(TagNewPage.nameInput, "tag_2");
+    await page.type(TagNewPage.descriptionInput, "description_tag_2");
+    await page.click(TagNewPage.saveButton);
+
+    console.log("And I wait for 1 seconds".padEnd(100, "_"));
+    await new Promise(r => setTimeout(r, 1000));
+
+    console.log('And I find a tag with "tag_2" name'.padEnd(100, "_"));
+    TagsPage.tagTest = "tag_2";
+    await page.click(DashboardPage.tagsMenu);
+    await page.isVisible(TagsPage.tagListItem); 
+    await page.screenshot({ path: pathReports + "./1.2-loginSuccess.png" });
+
+    console.log("Then I wait for 1 seconds")
+    await new Promise(r => setTimeout(r, 1000));
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    console.log("\n");
+    console.log("_".padEnd(100, "_"));
+    console.log("Scenario: 3. Actualizacion de un tag exitosa".padEnd(100, "_"));
+    console.log("Given I login on Ghost page with <ADMIN1> and <PASSWORD1>".padEnd(100,"_"));
+    await page.goto(urlAdmin);
+    if(await page.isVisible(LoginPage.emailInput)) {
       await page.type(LoginPage.emailInput, userAdmin);
       await page.type(LoginPage.passwInput, adminPass);
       await page.click(LoginPage.signInButton);
-      await new Promise((r) => setTimeout(r, 1000));
-      await page.screenshot({ path: pathReports + "./1.1-loginSuccess.png" });
     }
-    console.log("Login success".padEnd(100, "_"));
-  }
 
-  async function escenario_1(page) {
-    console.log("Scenario: 1. Creación de un tag exitoso".padEnd(100, "_"));
-    console.log("Given I login on Ghost page with <ADMIN1> and <PASSWORD1>".padEnd(100, "_"))
-    await login(page);
-  }
+    console.log(`When I create a new tag with tag_3 name and "description_tag_3" description`);
+    await page.click(DashboardPage.tagsMenu);
+    await page.click(TagsPage.newTagButton);
+    await page.type(TagNewPage.nameInput, "tag_3");
+    await page.type(TagNewPage.descriptionInput, "description_tag_3");
+    await page.click(TagNewPage.saveButton);
 
-  async function escenario_2(page) {
+    console.log("And I wait for 1 seconds".padEnd(100, "_"));
+    await new Promise(r => setTimeout(r, 1000));
+
+    console.log('And I find a tag with "tag_3" name');
+    TagsPage.tagTest = "tag_3";
+    await page.click(DashboardPage.tagsMenu);
+    await page.click(TagsPage.tagListItem);
+    
+    console.log('And I update a tag with "tag_3" slug and "description_tag_3_update" description');
+    await page.type(TagNewPage.descriptionInput, "description_tag_3_update");
+    await page.screenshot({ path: pathReports + "./1.3-loginSuccess.png" });    
+    await page.click(TagEditPage.saveButton);
+
+    console.log("Then I wait for 1 seconds")
+    await new Promise(r => setTimeout(r, 1000));
+
+    ////////////////////////////////////////////////////////////////////////////////////////
     console.log("\n");
-  }
-
-  async function escenario_3(page) {
+    console.log("_".padEnd(100, "_"));
+    console.log("Scenario: 4. Eliminacion de un tag exitoso".padEnd(100, "_"));
+    console.log("Given I login on Ghost page with <ADMIN1> and <PASSWORD1>".padEnd(100,"_"));
     await page.goto(urlAdmin);
-    if (await page.isVisible(LoginPage.emailInput)) await login(page);
-  }
+    if(await page.isVisible(LoginPage.emailInput)) {
+      await page.type(LoginPage.emailInput, userAdmin);
+      await page.type(LoginPage.passwInput, adminPass);
+      await page.click(LoginPage.signInButton);
+    }
 
-  async function escenario_4(page) {
-    await page.goto(urlAdmin);
-    if (await page.isVisible(LoginPage.emailInput)) await login(page);
-  }
+    console.log(`When I create a new tag with tag_4 name and "description_tag_4" description`);
+    await page.click(DashboardPage.tagsMenu);
+    await page.click(TagsPage.newTagButton);
+    await page.type(TagNewPage.nameInput, "tag_4");
+    await page.type(TagNewPage.descriptionInput, "description_tag_4");
+    await page.click(TagNewPage.saveButton);
 
-  async function escenario_5(page) {
-    await page.goto(urlAdmin);
-    if (await page.isVisible(LoginPage.emailInput)) await login(page);
+    console.log("And I wait for 1 seconds".padEnd(100, "_"));
+    await new Promise(r => setTimeout(r, 1000));
+
+    console.log('And I find a tag with "tag_4" name');
+    TagsPage.tagTest = "tag_4";
+    await page.click(DashboardPage.tagsMenu);
+    await page.click(TagsPage.tagListItem);
+
+    console.log("And I wait for 1 seconds".padEnd(100, "_"));
+    await new Promise(r => setTimeout(r, 1000));
+
+    console.log('And I deleted a tag with "tag_4" slug');
+    await page.click(TagEditPage.deleteButton);
+    await page.screenshot({ path: pathReports + "./1.4-loginSuccess.png" });
+    await page.click(TagEditPage.confirmDeleteButton);
+
+    console.log("Then I wait for 1 seconds".padEnd(100, "_"));
+    await new Promise(r => setTimeout(r, 1000));  
   }
 
   return;
--- playwrightGhost/pages_objects/tag_edit.page.js
+++ playwrightGhost/pages_objects/tag_edit.page.js
@@ -1,13 +1,18 @@
-const Page = require('./page');
+const Page = require("./page");
 
 class TagEditPage extends Page {
-
-  get nameInput() { return "input#tag-name" }
-  get saveButton() { return "section.view-actions button" }
- 
-  /* async open() {
-    await super.open('http://localhost:2368/ghost/#/editor/post')
-  } */
+  get nameInput() {
+    return "input#tag-name";
+  }
+  get saveButton() {
+    return "section.view-actions button";
+  }
+  get deleteButton() {
+    return 'button[class="gh-btn gh-btn-red gh-btn-icon"]';
+  }
+  get confirmDeleteButton() {
+    return 'button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]';
+  }
 }
 
-module.exports = new TagEditPage()
+module.exports = new TagEditPage();
